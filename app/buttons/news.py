e4a7b210a3dc1ac88aa7a566efae6f87b9970dcd
Moreez317 <alexander31777@gmail.com>
2021-04-25 21:34:41 +0300

    Add code comments

--- app/buttons/news.py
+++ app/buttons/news.py
@@ -3,23 +3,25 @@ class ButtonNews:
     def action(country):
         import requests
 
-
+        #creating request to newsAPI
         if country == 'ru':
             request_result = requests.get(
                 'https://newsapi.org/v2/top-headlines?country=ru&apiKey=7bc1b015bc614813ad32a62accce3cd1')
 
-        elif  country == 'de':
+        elif country == 'de':
             request_result = requests.get('https://newsapi.org/v2/top-headlines?country=de&apiKey=7bc1b015bc614813ad32a62accce3cd1')
 
+        #saves only json-encoded content from request
         request_result = request_result.json()
 
         output_string = ''
 
+        #verifying request result
         if request_result['status'] == 'ok':
+            #parsing json for 5 first headlines
             for i in range(5):
                 article = request_result['articles'][i]
                 headline = '   ' + article['title']
-                # print(output_string)
                 output_string = output_string + "\n" + headline
 
         return output_string
