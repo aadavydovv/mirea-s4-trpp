e3800a4c9d8fb7204a8a55a5136f87744ba54daa
senzaw <nigllemmesmash@gmail.com>
2021-04-25 20:49:11 +0300

    Create covid button

--- app/buttons/covid.py
+++ app/buttons/covid.py
@@ -1,5 +1,26 @@
-# заготовка
+import requests
+
+
 class ButtonCovid:
+
+    # действие кнопки; возвращает строку со статистикой COVID-19 по выбранной стране
     @staticmethod
     def action(country):
-        return f'covid {country}'
+
+        # актуальные данные по COVID-19 берем с сайта covid19api.com
+        url_ru = 'https://api.covid19api.com/total/country/russia'
+        url_de = 'https://api.covid19api.com/total/country/germany'
+
+        output = []
+        if country is "ru":
+            response = requests.get(url_ru)
+        else:
+            response = requests.get(url_de)
+        data = response.json()
+
+        # берём последнюю известную информацию из json файла
+        for key in data[-1]:
+            output.append(data[-1][key])
+
+        return f'Зарегистрировано случаев: {output[7]}\nСмертей: {output[8]}\nВыздоровело: {output[9]} \nАктивных ' \
+               f'случаев: {output[10]} '
