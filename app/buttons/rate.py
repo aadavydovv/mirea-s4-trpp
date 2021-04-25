0dc6e9a4107b3cfa60539519ee61af313bb07355
senzaw <nigllemmesmash@gmail.com>
2021-04-25 20:02:41 +0300

    Create rate button

--- app/buttons/rate.py
+++ app/buttons/rate.py
@@ -1,5 +1,17 @@
-# заготовка
+import requests
+
+
 class ButtonRate:
+
+    # действие кнопки; возвращает строку с курсом валюты выбранной страны
     @staticmethod
     def action():
-        return f'rate'
+
+        # информацию об актуальных курсах обмена валют берем с сайта ratesapi.io
+        url = 'https://api.ratesapi.io/api/latest?base=EUR'
+
+        response = requests.get(url)
+        data = response.json()
+        rate = round(data['rates']['RUB'], 3)
+
+        return f'Курс рубля к евро: {rate} RUB = 1 EUR'
