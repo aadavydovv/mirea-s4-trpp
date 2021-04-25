c21b49e36a7b325bdd1b271cb34a2ee10033e2bd
x <x43872@gmail.com>
2021-04-25 19:27:19 +0300

    Edit GUI
    Белоруссия заменена на Германию.
    * Изменены код страны и название его переменной соответственно.
    
    Теперь предполагается, что запрос курса выводит лишь отношение двух валют.
    * Изменена логика создания окна выбора (окна первого уровня).
    * Изменена логика создания окна результата.
    * Изменены переменные названия окон и связанных методов на более подходящие.

--- app/gui.py
+++ app/gui.py
@@ -14,7 +14,7 @@ class App:
 
     BG = '#434444'
     FG = '#FFFFFF'
-    CODE_BY = 'by'
+    CODE_DE = 'de'
     CODE_RU = 'ru'
     LEFT_MOUSE_BUTTON = "<Button-1>"
     PAD_X = 18
@@ -22,9 +22,9 @@ class App:
 
     # инициализация переменных окон
     def __init__(self):
-        self.window_main = tk.Tk()
-        self.window_choice = None
-        self.window_result = None
+        self.window_layer_0 = tk.Tk()
+        self.window_layer_1 = None
+        self.window_layer_2 = None
 
     # создание кнопки
     def make_button(self, text, master, action=None, font_size=12):
@@ -44,36 +44,40 @@ class App:
         return ttk.Label(master, text=text, foreground=self.FG, background=self.BG, font=font)
 
     # создание главного окна
-    def make_window_main(self):
-        self.window_main.configure(bg=self.BG)
-        self.window_main.resizable(False, False)
+    def make_window_layer_0(self):
+        self.window_layer_0.configure(bg=self.BG)
+        self.window_layer_0.resizable(False, False)
 
-        frame = tk.Frame(self.window_main, relief=tk.RAISED, background=self.BG)
+        frame = tk.Frame(self.window_layer_0, relief=tk.RAISED, background=self.BG)
         frame.grid(padx=self.PAD_X, pady=self.PAD_Y)
 
         label = self.make_label('Выберите запрос', frame)
         label.grid(column=0, row=0, columnspan=4, pady=self.PAD_Y)
 
-        button_news = self.make_button('Новости', frame, lambda event: self.make_window_choice('news'))
+        button_news = self.make_button('Новости', frame, lambda event: self.make_window_layer_1('news'))
         button_news.grid(column=0, row=1, padx=self.PAD_X, pady=self.PAD_Y)
 
-        button_weather = self.make_button('Погода', frame, lambda event: self.make_window_choice('weather'))
+        button_weather = self.make_button('Погода', frame, lambda event: self.make_window_layer_1('weather'))
         button_weather.grid(column=1, row=1, padx=self.PAD_X, pady=self.PAD_Y)
 
-        button_rate = self.make_button('Курс', frame, lambda event: self.make_window_choice('rate'))
+        button_rate = self.make_button('Курс', frame, lambda event: self.make_window_layer_1('rate'))
         button_rate.grid(column=2, row=1, padx=self.PAD_X, pady=self.PAD_Y)
 
-        button_covid = self.make_button('COVID-19', frame, lambda event: self.make_window_choice('covid'))
+        button_covid = self.make_button('COVID-19', frame, lambda event: self.make_window_layer_1('covid'))
         button_covid.grid(column=3, row=1, padx=self.PAD_X, pady=self.PAD_Y)
 
-    # создания окна выбора страны
-    def make_window_choice(self, action):
-        if self.window_choice:
-            self.window_choice.destroy()
+    # создания окна выбора страны или вывода курса
+    def make_window_layer_1(self, action):
+        if self.window_layer_1:
+            self.window_layer_1.destroy()
 
-        window = tk.Toplevel(self.window_main, bg=self.BG)
+        if action == 'rate':
+            self.make_window_result(lambda: ButtonRate().action(), layer_2=False)
+            return
+
+        window = tk.Toplevel(self.window_layer_0, bg=self.BG)
         window.resizable(False, False)
-        self.window_choice = window
+        self.window_layer_1 = window
 
         frame = tk.Frame(window, relief=tk.RAISED, background=self.BG)
         frame.grid(padx=self.PAD_X, pady=self.PAD_Y)
@@ -81,49 +85,49 @@ class App:
         label = self.make_label('Какая страна?', frame)
         label.grid(column=0, row=0, columnspan=2, pady=self.PAD_Y)
 
-        button_by = self.make_button('Белоруссия', frame)
-        button_by.grid(column=0, row=1, padx=self.PAD_X, pady=self.PAD_Y)
+        button_de = self.make_button('Германия', frame)
+        button_de.grid(column=0, row=1, padx=self.PAD_X, pady=self.PAD_Y)
 
         button_ru = self.make_button('Россия', frame)
         button_ru.grid(column=1, row=1, padx=self.PAD_X, pady=self.PAD_Y)
 
         if action == 'covid':
-            button_by.bind(self.LEFT_MOUSE_BUTTON,
-                           lambda event: self.make_window_result(lambda: ButtonCovid().action(self.CODE_BY)))
+            button_de.bind(self.LEFT_MOUSE_BUTTON,
+                           lambda event: self.make_window_result(lambda: ButtonCovid().action(self.CODE_DE)))
             button_ru.bind(self.LEFT_MOUSE_BUTTON,
                            lambda event: self.make_window_result(lambda: ButtonCovid().action(self.CODE_RU)))
         elif action == 'news':
-            button_by.bind(self.LEFT_MOUSE_BUTTON,
-                           lambda event: self.make_window_result(lambda: ButtonNews().action(self.CODE_BY)))
+            button_de.bind(self.LEFT_MOUSE_BUTTON,
+                           lambda event: self.make_window_result(lambda: ButtonNews().action(self.CODE_DE)))
             button_ru.bind(self.LEFT_MOUSE_BUTTON,
                            lambda event: self.make_window_result(lambda: ButtonNews().action(self.CODE_RU)))
-        elif action == 'rate':
-            button_by.bind(self.LEFT_MOUSE_BUTTON,
-                           lambda event: self.make_window_result(lambda: ButtonRate().action(self.CODE_BY)))
-            button_ru.bind(self.LEFT_MOUSE_BUTTON,
-                           lambda event: self.make_window_result(lambda: ButtonRate().action(self.CODE_RU)))
         elif action == 'weather':
-            button_by.bind(self.LEFT_MOUSE_BUTTON,
-                           lambda event: self.make_window_result(lambda: ButtonWeather().action(self.CODE_BY)))
+            button_de.bind(self.LEFT_MOUSE_BUTTON,
+                           lambda event: self.make_window_result(lambda: ButtonWeather().action(self.CODE_DE)))
             button_ru.bind(self.LEFT_MOUSE_BUTTON,
                            lambda event: self.make_window_result(lambda: ButtonWeather().action(self.CODE_RU)))
 
     # создание окна с ответом на запрос
-    def make_window_result(self, action):
-        if self.window_result:
-            self.window_result.destroy()
+    def make_window_result(self, action, layer_2=True):
+        if self.window_layer_2:
+            self.window_layer_2.destroy()
 
-        window = tk.Toplevel(self.window_choice, bg=self.BG, padx=self.PAD_X, pady=self.PAD_Y)
+        if layer_2:
+            window = tk.Toplevel(self.window_layer_1, bg=self.BG, padx=self.PAD_X, pady=self.PAD_Y)
+            self.window_layer_2 = window
+        else:
+            window = tk.Toplevel(self.window_layer_0, bg=self.BG, padx=self.PAD_X, pady=self.PAD_Y)
+            self.window_layer_1 = window
+
         window.resizable(False, False)
-        self.window_result = window
 
         label = self.make_label(action(), window, 12)
         label.pack()
 
     # выполнение приложения
     def run(self):
-        self.make_window_main()
-        self.window_main.mainloop()
+        self.make_window_layer_0()
+        self.window_layer_0.mainloop()
 
 
 if __name__ == '__main__':
